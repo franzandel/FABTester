75e8eca5baa37ca7f268aa6ef40498a03f4cfc9f
Franz Andel <franz.andel@amarbank.co.id>
2020-09-19 16:34:46 +0700

    Case solved (must use app instead of android in XML)

--- app/src/main/java/com/franzandel/fabtester/MainActivity.kt
+++ app/src/main/java/com/franzandel/fabtester/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.franzandel.fabtester
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Conclusion must use app:backgroundTint not android:backgroundTint
 
 //        fabTest.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
 
